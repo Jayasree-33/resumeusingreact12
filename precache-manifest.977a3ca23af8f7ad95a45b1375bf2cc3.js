7cc4353f440121bd64bbefdf55d4662ecc1368fe
Jayasree-33 <jayasreepotnuru93@gmail.com>
2020-10-05 13:40:29 +0530

    Updates

---
+++ precache-manifest.977a3ca23af8f7ad95a45b1375bf2cc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a451995f02921e328a1fa2bcc44c4b9e",
+    "url": "/resumeusingreact12/index.html"
+  },
+  {
+    "revision": "8ac7a55d5a8deeae501f",
+    "url": "/resumeusingreact12/static/css/main.31431516.chunk.css"
+  },
+  {
+    "revision": "22ceff8d7bca5b41c6cc",
+    "url": "/resumeusingreact12/static/js/2.7b7d1b9b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/resumeusingreact12/static/js/2.7b7d1b9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ac7a55d5a8deeae501f",
+    "url": "/resumeusingreact12/static/js/main.5d53b5b0.chunk.js"
+  },
+  {
+    "revision": "5c23aec216347a134e97",
+    "url": "/resumeusingreact12/static/js/runtime-main.c808ad0e.js"
+  },
+  {
+    "revision": "64c45fd8ee647ddbe381f93e09d21bc4",
+    "url": "/resumeusingreact12/static/media/ram.64c45fd8.jpg"
+  }
+]);
